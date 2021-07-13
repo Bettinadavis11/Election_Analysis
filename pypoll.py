ffbcf0da46874f7d55466b35053eeac02ab4c0ce
Bettinadavis11 <86321353+Bettinadavis11@users.noreply.github.com>
2021-07-12 17:45:06 -0700

    Update pypoll.py

--- pypoll.py
+++ pypoll.py
@@ -35,5 +35,3 @@ with open(file_to_load) as election_data:
     file_reader =  csv.reader(election_data)
     headers = next (file_reader)
     print(headers)
-    for row in file_reader:
-        print(row)
